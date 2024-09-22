9df811718f2fb7891294cda9d6a8d36fd0f3119b
Thomas <thomas.mirabile@ynov.com>
2024-09-22 23:12:16 +0200

    Add Cover test for register component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -7,8 +7,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
+import { AuthService } from '../../services/auth.service';
 
 import { RegisterComponent } from './register.component';
+import {of} from "rxjs";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -20,7 +22,7 @@ describe('RegisterComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -37,4 +39,50 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Check if form contains 4 required controls
+  it('should have a form with 4 controls', () => {
+    expect(component.form.contains('email')).toBeTruthy();
+    expect(component.form.contains('firstName')).toBeTruthy();
+    expect(component.form.contains('lastName')).toBeTruthy();
+    expect(component.form.contains('password')).toBeTruthy();
+  });
+
+// Ensure the email control is required
+  it('should make the email control required', () => {
+    const control = component.form.get('email');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+// Ensure the firstName control is required
+  it('should make the firstName control required', () => {
+    const control = component.form.get('firstName');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+// Ensure the lastName control is required
+  it('should make the lastName control required', () => {
+    const control = component.form.get('lastName');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+// Ensure the password control is required
+  it('should make the password control required', () => {
+    const control = component.form.get('password');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+// Verify error handling if user registration fails
+  it('should register user with error if the register request fails', () => {
+    const authService = TestBed.inject(AuthService);
+    const spy = jest.spyOn(authService, 'register').mockReturnValue(of());
+    component.submit();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
 });
