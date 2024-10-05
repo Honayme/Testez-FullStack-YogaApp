610e64319d823b21fe67bd251603e88dacde99ca
Thomas <thomas.mirabile@ynov.com>
2024-10-05 13:00:29 +0200

    BackEnd Add Exception tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTest.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.starterjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootSecurityJwtApplicationTest {
+    @Test
+    void contextLoads() {
+        // Ce test s'assure que le contexte Spring Boot se charge correctement
+    }
+}
